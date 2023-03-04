18f75298af8cc05a79b0c66a46aff19ed95ba358
drashti162002 <drashtikathrotiya.rao@gmail.com>
2023-03-04 17:26:49 +0530

    commit

--- js/index.js
+++ js/index.js
@@ -1,7 +1,10 @@
 const cellElements = document.querySelectorAll(".game_board .cell");
+const player1 = document.querySelector(".players .player1");
+const player2 = document.querySelector(".players .player2");
 
-const player1 = "O";
-const player2 = "X";
+
+const playerO = "O";
+const playerX = "X";
 
 const result = document.querySelector(".result");
 const result_text = document.querySelector(".result h2");
@@ -20,47 +23,44 @@ const winning_conditions =[
     [2, 4, 6]
 
 ]
-let toggleTurn;
+let toggleTurn = true;
 
 cellElements.forEach(cell=>{
     cell.onclick=()=>{ 
-        if(winner){
-            return;
-        }
-        else {
-        let currentPlayer = toggleTurn ? player1 : player2 ;
+        if(!winner){
+        console.log("this is winner");
+        let currentPlayer = toggleTurn ? playerO : playerX ;
         cell.classList.add("disabled");
         cell.innerHTML = currentPlayer;
         cell.classList.add(currentPlayer);
         //console.log(); 
         if(winnercheck(currentPlayer)){
+            console.log("first condition")
             console.log(currentPlayer + "winner");
-           
             winner = true;
-            result_text.innerHTML = currentPlayer + " Win the Game";
-            // if(currentPlayer){
-            //    winning_conditions = true;
-            // }
-            //cell.classList.add("disabled1");
-            // currentPlayer.innerHTML=""
+            if(winner){
+                //highlightCell(winner);
+                cell.classList.add("win_color");
+            }
+            result_text.innerHTML = currentPlayer + " Win The Game";
+            //result_text.innerHTML = "Game Is Over";
         }
         else if(isDraw()){
-           result_text.innerHTML = "Game is Draw"
+        
+            console.log("second condition")
+           result_text.innerHTML = "Game Is Draw"
            console.log("game is draw");
         } 
         swapPlayer();
-
     }
-}
+    }
 } )
-
 
 function selectplayer (player){
     console.log("THIS IS THE PLAYER", player)
     if(player == 1) toggleTurn = true
     else toggleTurn = false
 }
-
 
 function winnercheck(currentPlayer){
     return winning_conditions.some(condition=>{
@@ -73,22 +73,33 @@ function winnercheck(currentPlayer){
     })
 }
 
-function isDraw(){
-    if(!winner){
+// function highlightCell(winner){
+//     console.log("color");
+//     winner.forEach(index=>{
+//         cell[index].classList.add("color");
+//     });
+// }
 
-    
+
+
+
+
+function isDraw(){
     console.log(winner,"is winner");
     return [...cellElements].every(cell=>{
-        return cell.classList.contains(player1)  || cell.classList.contains(player2);
+        return cell.classList.contains(playerO)  || cell.classList.contains(playerX);
     })
-    }
-    else{
-        alert("restart the game");
-    }
 }
 
 function swapPlayer(){
     toggleTurn = !toggleTurn;
+    if(toggleTurn){
+        player1.classList.add("active");
+        player2.classList.remove("active");
+    }else{
+        player2.classList.add("active");
+        player1.classList.remove("active");
+    }
 
 }
 
